450f2682c0b4e182be1a0ffe97cc5d9806dca1e1
kerbless <bluburro7@gmail.com>
2020-10-01 07:54:32 +0200

    moved files from old folder in bluedeb (sh files)

---
+++ aircrack/handshakes.sh
@@ -0,0 +1,5 @@
+#!/bin/bahs
+
+mapfile -t array <Testfile
+echo "${array[0]}" # to demonstrate use of array values
+echo "${array[1]}"
---
+++ aircrack/outmon.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo airmon-ng stop wlp2s0mon
+sudo systemctl start network-manager
