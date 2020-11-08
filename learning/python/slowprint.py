a09cdafa03fa08aae1c46e16c53585d0d26961b6
kerbless <bluburro7@gmail.com>
2020-11-08 15:48:05 +0100

    Fix to get newline after every call

--- learning/python/slowprint.py
+++ learning/python/slowprint.py
@@ -4,6 +4,6 @@ def slowprint(string):
     for i in string:
         print(i, end="", flush=True)
         time.sleep(.1)
-
+    print("") #Saves the newline function at the end of every slowprint()
 text = "Hello, World!"
 slowprint(text)
