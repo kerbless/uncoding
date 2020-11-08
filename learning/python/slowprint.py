c4850571eea1dcf1b638e015ff2d0d4a35b30e71
kerbless <bluburro7@gmail.com>
2020-11-08 15:48:46 +0100

    formatting changes

--- learning/python/slowprint.py
+++ learning/python/slowprint.py
@@ -5,5 +5,6 @@ def slowprint(string):
         print(i, end="", flush=True)
         time.sleep(.1)
     print("") #Saves the newline function at the end of every slowprint()
+    
 text = "Hello, World!"
 slowprint(text)
