4f08a8a07d733d6e802f92679725df16365fc8f7
kerbless <bluburro7@gmail.com>
2021-02-11 11:21:04 +0100

    Finished SortVectFunc.c

--- school/Informatica/SortVectFunc.c
+++ school/Informatica/SortVectFunc.c
@@ -1,51 +1,64 @@
+// PREPROCESSOR
 #include <stdio.h>
 
-int* initVect(int vect_size); // Creates a vector from user input with determined size
-void printVect(int* vect, int vect_size); // Prints a vector using a pointer to first element
-int* sortMinMaxVect(int* vect, int vect_size); // Reorders an int vector from min to max
+// FUNCTION PROTOTYPES
+/* initializes a int vector from user input and returns a pointer to first element */
+int* initVect(int vect[], int vect_size); 
 
+/* prints a vector using a pointer to first element */
+void printVect(int* pvect, int vect_size);
+
+/* reorders an int vector from min to max using a pointer to first element */
+void sortMinMaxVect(int* pvect, int vect_size);
+
+// MAIN
 int main() {
-	int vect_size;
-    int* pvect;
-
-    /* get vector size */
-    printf("Inserisci il numero di elementi interi da salvare: "); scanf("%d", &vect_size);
+    /* get vector size and create it */
+    int vect_size;
+    printf("[IN] Insert the integer vector size: "); scanf("%d", &vect_size);
 	int vect[vect_size];
 
-    pvect = initVect(vect_size);
+    /* initialize vector from user input and get pointer */
+    int* pvect = initVect(vect, vect_size);
+    printVect(pvect, vect_size); //let's see our new vector
 
-	printf("\nOrdinato: "); printVect(vect, vect_size);
-	return 0;
+    /* sort vector */
+    sortMinMaxVect(pvect, vect_size);
+    printf("\n[i] Sorting finished.\n"); 
+    printVect(pvect, vect_size); //let's see our vector sorted
+
+    /* end */
+    return 0;
 }
-
-int* initVect(int vect_size) {
-    int i, vect[];
+ 
+// FUNCTION DEFINITIONS
+int* initVect(int vect[], int vect_size) {
+	int i; // support for older gcc versions
     for (i = 0; i < vect_size; i++) {
-		printf("Inserisci l'elemento di indice [%d]: ", i); scanf("%d", &vect[i]);
+		printf("[IN] Insert the integer indexed [%d]: ", i); scanf("%d", &vect[i]);
 	}
+    return &vect[0];
 }
 
-void printVect(int* pvect, int vect_size); {
-	/* Gets an int vector and it's size and prints it's values in line*/
-	int i; // Support for older gcc versions
-    printf("Vettore: |");
+void printVect(int* pvect, int vect_size) {
+	int i; // support for older gcc versions
+    printf("[OUT] Vector: |");
 	for (i = 0; i < vect_size; i++) {
 		printf("%d|", *(pvect + i));
 	}
 }
 
-int* sortMinMaxVect(int* vect, int vect_size) {
+void sortMinMaxVect(int* pvect, int vect_size) {
+    printf("\n[i] Sorting vector...");
+    int i, j; // support for older gcc versions
     for (i = 0; i < vect_size - 1; i++) {
 		for (j = i + 1; j < vect_size; j++) {
-			/*printf("\n\n_____debug_____");
-			printf("\n1. vect[%d] %d", i, vect[i]);
-			printf("\n2. vect[%d]: %d", j,  vect[j]);
-			printf("\nConfronto: %d < %d", vect[j], vect[i]);*/
-			if (vect[j] < vect[i]) {
-				//printf("\nConfronto VERO"); // still debug stuff
-				int tmp = vect[i];
-				vect[i] = vect[j];
-				vect[j] = tmp;
+			if (*(pvect + j) < *(pvect + i)) {
+				printf("\n--> [i] Switched element %d indexed %d with element %d indexed %d", *(pvect + i), i, *(pvect + j), j);
+                printf(" -> "); printVect(pvect, vect_size);
+                int tmp = *(pvect + i);
+				*(pvect + i) = *(pvect + j);
+				*(pvect + j) = tmp;
 			}
 		}
 	}
