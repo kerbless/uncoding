b5e9d4a1c50d5cc8ce24f2e66543f82180945418
kerbless <bluburro7@gmail.com>
2021-02-11 09:28:45 +0100

    Update SortVect.c

--- school/Informatica/SortVect.c
+++ school/Informatica/SortVect.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 void printVect(int vect[], int vect_size) {
-	int i;
+	/* Gets an int vector and it's size and prints it's values in line*/
+	int i; // Support for older gcc versions
 	printf("|");
 	for (i = 0; i < vect_size; i++) {
 		printf("%d|", vect[i]);
@@ -9,7 +10,9 @@ void printVect(int vect[], int vect_size) {
 }
 
 int main() {
-	int vect_size, i, j;
+	/* Variables Gets an int vector and it's size and prints it's values in line*/
+	int vect_size, i, j; 
+	
 	printf("Inserisci il numero di elementi interi da salvare: "); scanf("%d", &vect_size);
 	int vect[vect_size];
 	for (i = 0; i < vect_size; i++) {
@@ -17,12 +20,13 @@ int main() {
 	}
 	printf("Vettore:  "); printVect(vect, vect_size);
 	
+	/* Reorder */
 	for (i = 0; i < vect_size - 1; i++) {
 		for (j = i + 1; j < vect_size; j++) {
-			printf("\n\n_____debug_____");
+			/*printf("\n\n_____debug_____");
 			printf("\n1. vect[%d] %d", i, vect[i]);
 			printf("\n2. vect[%d]: %d", j,  vect[j]);
-			printf("\nConfronto: %d < %d", vect[j], vect[i]);		
+			printf("\nConfronto: %d < %d", vect[j], vect[i]);*/
 			if (vect[j] < vect[i]) {
 				printf("\nConfronto VERO");
 				int tmp = vect[i];
